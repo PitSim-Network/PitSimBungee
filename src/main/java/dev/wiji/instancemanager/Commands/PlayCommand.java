cf4519046e1fccebdfb067d3cfc056e52f67f0e1
wiji <wijigaming.yt@gmail.com>
2022-10-12 16:05:47 -0700

    Added save protection to play command Added cooldown to play command

--- src/main/java/dev/wiji/instancemanager/Commands/PlayCommand.java
+++ src/main/java/dev/wiji/instancemanager/Commands/PlayCommand.java
@@ -1,20 +1,31 @@
 package dev.wiji.instancemanager.Commands;
 
 import dev.wiji.instancemanager.BungeeMain;
+import dev.wiji.instancemanager.Objects.PitSimServer;
+import dev.wiji.instancemanager.Objects.PluginMessage;
+import dev.wiji.instancemanager.Objects.ServerStatus;
 import dev.wiji.instancemanager.PitSim.PitSimServerManager;
+import dev.wiji.instancemanager.ProxyRunnable;
 import dev.wiji.instancemanager.Skywars.SkywarsGameManager;
 import dev.wiji.instancemanager.Skywars.SkywarsQueueManager;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.connection.Server;
 import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.Plugin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class PlayCommand extends Command {
 	public PlayCommand(Plugin bungeeMain) {
 		super("play");
 	}
+
+	public static List<ProxiedPlayer> cooldownPlayers = new ArrayList<>();
 
 	@Override
 	public void execute(CommandSender commandSender, String[] strings) {
@@ -25,10 +36,42 @@ public class PlayCommand extends Command {
 			return;
 		}
 
+		ProxiedPlayer player = ((ProxiedPlayer) commandSender);
+
+		if(cooldownPlayers.contains(player)) {
+			player.sendMessage((new ComponentBuilder("This command is on cooldown!").color(ChatColor.RED).create()));
+			return;
+		}
+
+		cooldownPlayers.add(player);
+		((ProxyRunnable) () -> cooldownPlayers.remove(player)).runAfter(5, TimeUnit.SECONDS);
+
 		if(strings[0].equalsIgnoreCase("pitsim")) {
-			if(!PitSimServerManager.queue((ProxiedPlayer) commandSender, 0)) {
-				commandSender.sendMessage((new ComponentBuilder("There are currently no available servers!").color(ChatColor.RED).create()));
+
+
+			Server currentServer = player.getServer();
+
+			if(currentServer.getInfo().getName().contains("pitsim")) {
+				boolean canChange = false;
+				for(PitSimServer pitSimServer : PitSimServerManager.serverList) {
+					if(pitSimServer.getServerInfo() == currentServer.getInfo()) continue;
+					if(pitSimServer.status != ServerStatus.RUNNING) continue;
+					canChange = true;
+				}
+
+				if(!canChange) {
+					commandSender.sendMessage((new ComponentBuilder("There are currently no other available servers!").color(ChatColor.RED).create()));
+					return;
+				}
+
+				commandSender.sendMessage((new ComponentBuilder("Looking for a server...").color(ChatColor.GREEN).create()));
+				new PluginMessage().writeString("SAVE DATA").writeString(player.getName()).addServer(currentServer.getInfo()).send();
+				return;
+
 			}
+
+			PitSimServerManager.queue((ProxiedPlayer) commandSender, 0);
+
 		} else if(strings[0].equalsIgnoreCase("sync")) {
 			commandSender.sendMessage((new ComponentBuilder("Sending you to Sync!").color(ChatColor.GREEN).create()));
 		((ProxiedPlayer) commandSender).connect(BungeeMain.INSTANCE.getProxy().getServerInfo("sync"));
