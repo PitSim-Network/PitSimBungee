fddaf5d8173bfbcd99976c30876152e224247b78
KyroKrypt <kyrokrypt@gmail.com>
2023-02-26 00:30:06 -0800

    discord oauth

--- src/main/java/dev/wiji/instancemanager/discord/DiscordManager.java
+++ src/main/java/dev/wiji/instancemanager/discord/DiscordManager.java
@@ -102,7 +102,7 @@ public class DiscordManager implements EventListener {
 			String username = "***REMOVED***";
 			String password = "***REMOVED***";
 			return DriverManager.getConnection(dbUrl, username, password);
-		} catch(Exception ignored) {} ;
+		} catch(Exception ignored) {}
 		return null;
 	}
 
