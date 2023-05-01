b604fd444e546b2021af40d46052d2bd3f7a3ead
wiji <wijigaming.yt@gmail.com>
2023-05-01 14:40:44 -0700

    Log enum update

--- src/main/java/dev/wiji/instancemanager/enums/DiscordLogChannel.java
+++ src/main/java/dev/wiji/instancemanager/enums/DiscordLogChannel.java
@@ -3,6 +3,7 @@ package dev.wiji.instancemanager.enums;
 // Enum needs to be mirrored in the pitsim plugin
 public enum DiscordLogChannel {
 	GOD_MENU_LOG_CHANNEL(1091234311291805837L),
+	TUTORIAL_LOG_CHANNEL(1102710878433980456L),
 	;
 
 	private final Long channelID;
