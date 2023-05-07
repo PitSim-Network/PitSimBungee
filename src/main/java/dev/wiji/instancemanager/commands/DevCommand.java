8841ec38a5578dac6f4d7b50181bd8318e1e8e6c
wiji <wijigaming.yt@gmail.com>
2023-05-06 21:48:37 -0700

    Fixed Dev command

--- src/main/java/dev/wiji/instancemanager/commands/DevCommand.java
+++ src/main/java/dev/wiji/instancemanager/commands/DevCommand.java
@@ -1,6 +1,9 @@
 package dev.wiji.instancemanager.commands;
 
+import dev.wiji.instancemanager.BungeeMain;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.Plugin;
@@ -14,8 +17,11 @@ public class DevCommand extends Command {
 
 	@Override
 	public void execute(CommandSender commandSender, String[] strings) {
-		if(!(commandSender instanceof ProxiedPlayer)) return;
+		if(!isEnabled && !commandSender.hasPermission("pitsim.toggle")) {
+			commandSender.sendMessage((new ComponentBuilder("Access to this server is currently disabled!").color(ChatColor.RED).create()));
+			return;
+		}
 
-
+		((ProxiedPlayer) commandSender).connect(BungeeMain.INSTANCE.getProxy().getServerInfo("dev2"));
 	}
 }
